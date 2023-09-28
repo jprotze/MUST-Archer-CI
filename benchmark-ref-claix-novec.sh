b8c54d287d8b3cce3b4e3dc849ccb029b6c836ea
Joachim Jenke <jenke@itc.rwth-aachen.de>
2023-09-28 08:52:04 +0200

    Adding more runscripts

---
+++ benchmark-ref-claix-novec.sh
@@ -0,0 +1,10 @@
+module load clang/15-fiber-novec CMake Ninja Python JUBE Tk
+cd code/detach-deps/jube/
+jube run jube-claix-fiber.xml --tag 14
+jube analyse bench_run
+jube result bench_run > result-14.txt
+python3 gen_diag.py result-14.txt -14
+jube run jube-claix-fiber.xml --tag 15
+jube analyse bench_run
+jube result bench_run > result-15.txt
+python3 gen_diag.py result-15.txt -15
